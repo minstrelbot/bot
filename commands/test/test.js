b70e1411ddb41d9640d6168a66749e01d5b6366d
Shadow <thebotoftheshadow@gmail.com>
2021-11-02 12:20:55 -0500

    stuff i guess

--- commands/test/test.js
+++ commands/test/test.js
@@ -1,4 +1,83 @@
-const { createAudioPlayer, createAudioResource, NoSubscriberBehavior, joinVoiceChannel, AudioPlayerStatus } = require("@discordjs/voice")
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, StreamType, AudioPlayerStatus, VoiceConnectionStatus } = require("@discordjs/voice")
+
+/**
+ * 	In this example, we are creating a single audio player that plays to a number of voice channels.
+ * The audio player will play a single track.
+ */
+
+/**
+ * Create the audio player. We will use this for all of our connections.
+ */
+const player = createAudioPlayer()
+
+function playSong() {
+  /**
+   * Here we are creating an audio resource using a sample song freely available online
+   * (see https://www.soundhelix.com/audio-examples)
+   *
+   * We specify an arbitrary inputType. This means that we aren't too sure what the format of
+   * the input is, and that we'd like to have this converted into a format we can use. If we
+   * were using an Ogg or WebM source, then we could change this value. However, for now we
+   * will leave this as arbitrary.
+   */
+  const resource = createAudioResource("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", {
+    inputType: StreamType.Arbitrary,
+  })
+
+  /**
+   * We will now play this to the audio player. By default, the audio player will not play until
+   * at least one voice connection is subscribed to it, so it is fine to attach our resource to the
+   * audio player this early.
+   */
+  player.play(resource)
+
+  /**
+   * Here we are using a helper function. It will resolve if the player enters the Playing
+   * state within 5 seconds, otherwise it will reject with an error.
+   */
+  return entersState(player, AudioPlayerStatus.Playing, 5e3)
+}
+
+async function connectToChannel(channel) {
+  /**
+   * Here, we try to establish a connection to a voice channel. If we're already connected
+   * to this voice channel, @discordjs/voice will just return the existing connection for us!
+   */
+  const connection = joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  })
+
+  //if (channel.type == "GUILD_STAGE_VOICE") interaction.guild.me.voice.setSuppressed(false)
+
+  /**
+   * If we're dealing with a connection that isn't yet Ready, we can set a reasonable
+   * time limit before giving up. In this example, we give the voice connection 30 seconds
+   * to enter the ready state before giving up.
+   */
+  try {
+    /**
+     * Allow ourselves 30 seconds to join the voice channel. If we do not join within then,
+     * an error is thrown.
+     */
+    await entersState(connection, VoiceConnectionStatus.Ready, 10e3)
+    /**
+     * At this point, the voice connection is ready within 30 seconds! This means we can
+     * start playing audio in the voice channel. We return the connection so it can be
+     * used by the caller.
+     */
+    return connection
+  } catch (error) {
+    /**
+     * At this point, the voice connection has not entered the Ready state. We should make
+     * sure to destroy it, and propagate the error by throwing it, so that the calling function
+     * is aware that we failed to connect to the channel.
+     */
+    connection.destroy()
+    throw error
+  }
+}
 
 module.exports = {
   command: {
@@ -10,32 +89,16 @@ module.exports = {
 }
 
 module.exports.run = async (interaction, client) => {
-  interaction.reply("Queued")
-  let connection
-  if (!interaction.member.voice.channel) return
-  connection = await joinVoiceChannel({
-    channelId: interaction.member.voice.channel.id,
-    guildId: interaction.guild.id,
-    adapterCreator: interaction.guild.voiceAdapterCreator,
-  })
-  if (interaction.member.voice.channel.type == "stage") guild.me.voice.setSuppressed(false)
+  interaction.deferReply()
 
-  const player = await createAudioPlayer({
-    behaviors: {
-      noSubscriber: NoSubscriberBehavior.Pause,
-    },
-  })
+  const channel = interaction.member?.voice.channel
+  if (!channel) return
 
-  await connection.subscribe(player)
-
-  const resource = createAudioResource("./piano.mp3")
-  player.play(resource)
-
-  player.on(AudioPlayerStatus.Playing, () => {
-    console.log("The audio player has started playing!")
-  })
-
-  player.on(AudioPlayerStatus.AutoPaused, () => {
-    console.log("The audio player has stopped playing!")
-  })
+  try {
+    const connection = await connectToChannel(channel)
+    connection.subscribe(player)
+    await interaction.editReply("Playing now!")
+  } catch (error) {
+    console.error(error)
+  }
 }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const fs = require("fs")
 const Discord = require("discord.js")
 const client = new Discord.Client({
-  intents: ["GUILDS", "GUILD_MESSAGES"],
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"],
 })
 const config = require("./config")
 
