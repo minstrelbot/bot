3283ebe30b2ec3cd2a0273e814e60544e9232aae
Shadow <william.shadow@outlook.com>
2022-03-01 14:27:47 -0600

    patch getting stories

--- stories.js
+++ stories.js
@@ -4,7 +4,7 @@ const { Collection } = require("discord.js")
 const storyCol = new Collection()
 fs.readdir("./stories/", (err, folders) => {
   folders?.forEach((storyFolder) => {
-    if(storyFolder.startsWith(".")) return
+    if (storyFolder.startsWith(".")) return
     let story = generateStory(storyFolder)
     if (!story.id || !story.name) return console.log(`Unable to get the story --${storyFolder}-- as it is missing data`)
     console.log(`Loaded the ${story.name} story with an ID of ${story.id}`)
@@ -14,8 +14,13 @@ fs.readdir("./stories/", (err, folders) => {
 
 const generateStory = (storyFolder) => {
   let story = {}
-  let info = matter.read(`./stories/${storyFolder}/MAIN.md`)
-  story = info.data
+  let info
+  try {
+    info = matter.read(`./stories/${storyFolder}/MAIN.md`)
+  } catch {
+    info = {}
+  }
+  story = info?.data || {}
   story.folder = `${__dirname}/stories/${storyFolder}`
   story.description = info.content
   return story
